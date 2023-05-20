e267368fdb3a40bffe5965384ebbde73aae3c5ef
Clifford Hepplethwaite <cliffordrh@gmail.com>
2023-05-20 19:15:48 +0200

    Log out template styled

--- job_proj/urls.py
+++ job_proj/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path,include
 from users import views as user_views
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("register/",user_views.register, name="register"),
+    path("register/", user_views.register, name="register"),
+    path("log-in/", auth_views.LoginView.as_view(template_name='users/login.html'), name="log_in"),
+    path("log-out/", auth_views.LogoutView.as_view(template_name='users/logout.html'), name="log_out"),
     path('',include('job_app.urls')),
 ]
--- users/views.py
+++ users/views.py
@@ -10,8 +10,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('job_list')
+            messages.success(request, f'Your account has been created, {username}! You are now able to log in.')
+            return redirect('log_in')
     else:
         form = UserRegistrationForm()
     return render(request,'users/register.html',{'form':form})
