00ff890b89770faffaad7158c4e7b2272043ffb5
Clifford Hepplethwaite <cliffordrh@gmail.com>
2023-05-21 02:29:58 +0200

    url fix

--- job_proj/urls.py
+++ job_proj/urls.py
@@ -4,7 +4,7 @@ from users import views as user_views
 from django.contrib.auth import views as auth_views
 urlpatterns = [
     path("samisongo1986_ykw/", admin.site.urls),
-    path("register/", user_views.register, name="register"),
+    path("sign-up/", user_views.register, name="register"),
     path("log-in/", auth_views.LoginView.as_view(template_name='users/login.html'), name="log_in"),
     path("log-out/", auth_views.LogoutView.as_view(template_name='users/logout.html'), name="log_out"),
     path('',include('job_app.urls')),
