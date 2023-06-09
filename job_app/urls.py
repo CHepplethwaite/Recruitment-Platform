170c3f6d76d07a60b13602594e5d10645b8e3494
Clifford Hepplethwaite <cliffordrh@gmail.com>
2023-06-09 05:16:11 +0200

    Updated url for details

--- job_app/urls.py
+++ job_app/urls.py
@@ -4,15 +4,12 @@ from . import views as job_views
 urlpatterns =[
     path('',job_views.jobListView.as_view(), name='job_list'),
     # path('jobs/',job_views.jobListView.as_view(), name='job-list'),
-    path('jobs/<int:pk>/<slug:slug>/', job_views.jobDetailView.as_view(),name="job_detail"),
+    path('job-details/<int:pk>/<slug:slug>/', job_views.jobDetailView.as_view(),name="job_detail"),
     path('about/', job_views.about, name="about"),
     path('career-advice/', job_views.career_advice, name="career_advice"),
     path('contact/', job_views.contact, name="contact"),
     path('education/',job_views.education, name="education"),
     path('disclaimer/', job_views.legal, name="legal"),
-    path('payroll/', job_views.payroll, name="payroll"),
-    path('recruitment/', job_views.recruitment, name="recruitment"),
-    path('submit-resume/', job_views.submit_resume, name="submit_resume"),
    
    # category urls
     path('job-categories/academia-and-research/',job_views.academiaListView.as_view(),name="academia"),
