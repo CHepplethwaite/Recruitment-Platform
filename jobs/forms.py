9d9c7fb9f28b34ca6975e5fcaca8406e70f8d7b0
Clifford Hepplethwaite <clifford@thinkpad.localhost>
2024-01-18 19:29:34 +0200

    Added field

--- jobs/forms.py
+++ jobs/forms.py
@@ -5,10 +5,11 @@ from job_app.models import job
 class JobForm(forms.ModelForm):
     class Meta:
         model = job
-        fields = ['details', 'job_title', 'closing_date', 'district', 'province', 'industry', 'logo']
+        fields = ['details','job_title','employment_type', 'closing_date', 'district', 'province', 'industry', 'logo']
         widgets = {
             'details': forms.Textarea(attrs={'class': 'form-control'}),
             'job_title': forms.TextInput(attrs={'class': 'form-control'}),
+            'employment_type': forms.Select(attrs={'class': 'form-control'}),
             'closing_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
             'district': forms.Select(attrs={'class': 'form-control'}),
             'province': forms.Select(attrs={'class': 'form-control'}),
