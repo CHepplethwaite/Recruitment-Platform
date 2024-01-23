181d15c1e7eeb204fc971264da9d86de04a19c9f
Clifford Hepplethwaite <clifford@thinkpad.localhost>
2024-01-23 21:20:45 +0200

    WIP JOB TEMPLATES

--- jobs/urls.py
+++ jobs/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views as job_views
 
 urlpatterns = [
-    path('jobs/create-job/<int:pk>/', job_views.jobCreateView.as_view(), name="create_job"),
+    path('jobs/create-job/', job_views.jobCreateView.as_view(), name="create_job"),
     path('jobs/<int:pk>/delete/', job_views.jobDeleteView.as_view(), name="delete_job"),
     path('jobs/<int:pk>/update/', job_views.jobUpdateView.as_view(), name="edit_job"),
     path('jobs/', job_views.jobsListView.as_view(), name="job_list_admin_panel"),
