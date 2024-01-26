ca6601db49e3537577f672b384a3e2540c0e456e
Clifford Hepplethwaite <clifford@thinkpad.localhost>
2024-01-27 01:26:19 +0200

    Admin

--- job_app/admin.py
+++ job_app/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from .models import job
 
 class jobAdmin(admin.ModelAdmin):
-    list_display=("details","user",'job_title',"employment_type","industry","post_date","closing_date","logo","status")
-    list_filter=("status","post_date","closing_date")
-    search_fields=("details","user",'job_title',"employment_type","industry","post_date","closing_date","logo","status")
+    list_display=("status","details","user",'country','job_title',"employment_type","industry","post_date","closing_date","logo",)
 
 admin.site.register(job,jobAdmin)
