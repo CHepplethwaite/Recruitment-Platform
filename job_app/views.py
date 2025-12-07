11bb1c6a733a1dd42d2e62cfb3ad40acdf9d88e8
Clifford Hepplethwaite <cliffordrh@gmail.com>
2025-12-07 06:50:55 +0200

    Improved implementation of ordering

--- job_app/views.py
+++ job_app/views.py
@@ -41,8 +41,7 @@ def submit_resume(request):
 class jobListView(ListView):
     model = job
     paginate_by = 10
-    ordering = ['-post_date']
-    queryset=job.objects.filter(status__exact=f"{True}")
+    queryset=job.objects.filter(status__exact=f"{True}").order_by('-post_date')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -70,18 +69,16 @@ class jobDetailView(DetailView):
 class academiaListView(ListView):
     model = job
     paginate_by = 10
-    ordering = ['-post_date']
     template_name = 'job_app/categories/academia.html'
-    queryset=job.objects.filter(industry__exact="ACADEMIA", status__exact=f"{True}")
+    queryset=job.objects.filter(industry__exact="ACADEMIA", status__exact=f"{True}").order_by('-post_date')
     
 
 
 class adminListView(ListView):
     model = job
     paginate_by = 10
-    ordering = ['-post_date']
     template_name = 'job_app/categories/administration_list.html'
-    queryset=job.objects.filter(industry__exact="ADMINISTRATION", status__exact=f"{True}")
+    queryset=job.objects.filter(industry__exact="ADMINISTRATION", status__exact=f"{True}").order_by('-post_date')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -101,9 +98,8 @@ class adminListView(ListView):
 class accountancyListView(ListView):
     model = job
     paginate_by = 10
-    ordering = ['-post_date']
     template_name = 'job_app/categories/accountancy_list.html'
-    queryset=job.objects.filter(industry__exact="ACCOUNTANCY", status__exact=f"{True}")
+    queryset=job.objects.filter(industry__exact="ACCOUNTANCY", status__exact=f"{True}").order_by('-post_date')
   
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -123,9 +119,8 @@ class accountancyListView(ListView):
 class agricultureListView(ListView):
     model = job
     paginate_by = 10
-    ordering = ['-post_date']
     template_name = 'job_app/categories/agriculture_list.html'
-    queryset=job.objects.filter(industry__exact="AGRICULTURE", status__exact=f"{True}")
+    queryset=job.objects.filter(industry__exact="AGRICULTURE", status__exact=f"{True}").order_by('-post_date')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -145,9 +140,8 @@ class agricultureListView(ListView):
 class bankingListView(ListView):
     model = job
     paginate_by = 10
-    ordering = ['-post_date']
     template_name = 'job_app/categories/banking_and_finance.html'
-    queryset=job.objects.filter(industry__exact="BANKING_AND_FINANCE", status__exact=f"{True}")
+    queryset=job.objects.filter(industry__exact="BANKING_AND_FINANCE", status__exact=f"{True}").order_by('-post_date')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -167,9 +161,8 @@ class bankingListView(ListView):
 class developmentListView(ListView):
     model = job
     paginate_by = 10
-    ordering = ['-post_date']
     template_name = 'job_app/categories/development_list.html'
-    queryset=job.objects.filter(industry__exact="DEVELOPMENT", status__exact=f"{True}")
+    queryset=job.objects.filter(industry__exact="DEVELOPMENT", status__exact=f"{True}").order_by('-post_date')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -191,9 +184,8 @@ class developmentListView(ListView):
 class engineeringListView(ListView):
     model = job
     paginate_by = 10
-    ordering = ['-post_date']
     template_name = 'job_app/categories/engineering_and_construction_list.html'
-    queryset=job.objects.filter(industry__exact="ENGINEERING_AND_CONSTRUCTION", status__exact=f"{True}")
+    queryset=job.objects.filter(industry__exact="ENGINEERING_AND_CONSTRUCTION", status__exact=f"{True}").order_by('-post_date')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -216,9 +208,8 @@ class engineeringListView(ListView):
 class healthListView(ListView):
     model = job
     paginate_by = 10
-    ordering = ['-post_date']
     template_name = 'job_app/categories/health_list.html'
-    queryset=job.objects.filter(industry__exact="HEALTH", status__exact=f"{True}")
+    queryset=job.objects.filter(industry__exact="HEALTH", status__exact=f"{True}").order_by('-post_date')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -239,9 +230,8 @@ class healthListView(ListView):
 class hrListView(ListView):
     model = job
     paginate_by = 10
-    ordering = ['-post_date']
     template_name = 'job_app/categories/human_resource_list.html'
-    queryset=job.objects.filter(industry__exact="HUMAN_RESOURCE", status__exact=f"{True}")
+    queryset=job.objects.filter(industry__exact="HUMAN_RESOURCE", status__exact=f"{True}").order_by('-post_date')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -262,9 +252,8 @@ class hrListView(ListView):
 class lawListView(ListView):
     model = job
     paginate_by = 10
-    ordering = ['-post_date']
     template_name = 'job_app/categories/legal_list.html'
-    queryset=job.objects.filter(industry__exact="LAW", status__exact=f"{True}")
+    queryset=job.objects.filter(industry__exact="LAW", status__exact=f"{True}").order_by('-post_date')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -285,9 +274,8 @@ class lawListView(ListView):
 class manufacturingListView(ListView):
     model = job
     paginate_by = 10
-    ordering = ['-post_date']
     template_name = 'job_app/categories/manufacturing_list.html'
-    queryset=job.objects.filter(industry__exact="MANUFACTURING_FMCG", status__exact=f"{True}")
+    queryset=job.objects.filter(industry__exact="MANUFACTURING_FMCG", status__exact=f"{True}").order_by('-post_date')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -308,9 +296,8 @@ class manufacturingListView(ListView):
 class miscListView(ListView):
     model = job
     paginate_by = 10
-    ordering = ['-post_date']
     template_name = 'job_app/categories/other_list.html'
-    queryset=job.objects.filter(industry__exact="OTHER", status__exact=f"{True}")
+    queryset=job.objects.filter(industry__exact="OTHER", status__exact=f"{True}").order_by('-post_date')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -331,9 +318,8 @@ class miscListView(ListView):
 class retailListView(ListView):
     model = job
     paginate_by = 10
-    ordering = ['-post_date']
     template_name = 'job_app/categories/retail_and_sales.html'
-    queryset=job.objects.filter(industry__exact="RETAIL_AND_SALES", status__exact=f"{True}")
+    queryset=job.objects.filter(industry__exact="RETAIL_AND_SALES", status__exact=f"{True}").order_by('-post_date')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -354,9 +340,8 @@ class retailListView(ListView):
 class techListView(ListView):
     model = job
     paginate_by = 10
-    ordering = ['-post_date']
     template_name = 'job_app/categories/technology_list.html'
-    queryset=job.objects.filter(industry__exact="ICT_AND_TELCO", status__exact=f"{True}")
+    queryset=job.objects.filter(industry__exact="ICT_AND_TELCO", status__exact=f"{True}").order_by('-post_date')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -376,9 +361,8 @@ class techListView(ListView):
 class publicSectorListView(ListView):
     model = job
     paginate_by = 10
-    ordering = ['-post_date']
     template_name = 'job_app/categories/public_sector_list.html'
-    queryset=job.objects.filter(industry__exact="PUBLIC_SECTOR", status__exact=f"{True}")
+    queryset=job.objects.filter(industry__exact="PUBLIC_SECTOR", status__exact=f"{True}").order_by('-post_date')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -398,9 +382,8 @@ class publicSectorListView(ListView):
 class transportListView(ListView):
     model = job
     paginate_by = 10
-    ordering = ['-post_date']
     template_name = 'job_app/categories/transportation_and_logistics_list.html'
-    queryset=job.objects.filter(industry__exact="TRANSPORT_AND_LOGISTICS", status__exact=f"{True}")
+    queryset=job.objects.filter(industry__exact="TRANSPORT_AND_LOGISTICS", status__exact=f"{True}").order_by('-post_date')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
